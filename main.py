bb90200c21e66abc131d7655eecea7239d799018
danial2007-btk <danial.btkit@gmail.com>
2024-03-11 13:31:34 +0500

    Updated Code for AItools

---
+++ main.py
@@ -0,0 +1,46 @@
+from bson import ObjectId
+import io
+
+from carDetection import imageChecker
+from NLPengine import descriptionChecker
+from dataPipeline import getData
+
+def carAdMain(carID):
+    try:
+        # check the car data
+        carData = getData(carID)
+
+        if carData == None:
+            return {"Response": "The data of the requested Id is not available"}
+
+        if carData[0].get('adStatus') != "Pending":
+            return {"Response": "The Car AdStatus is not in Pending State"}
+
+        ID = carData[0].get('Id')
+        carImages = carData[0].get('images')
+        carDesc = carData[0].get('description')
+
+        if carImages is not None:
+            imageRes = [imageChecker(image) for image in carImages]
+
+            filtered_carImage = [img for img, result in zip(carImages, imageRes) if result == 1]
+
+            # Find rejected images
+            rejectedImages = list(set(carImages) - set(filtered_carImage))
+            
+            if len(filtered_carImage) == 0:
+                return {'Id': ID, 'CheckedDescription': carDesc, 'rejectedImages':rejectedImages,'adStatus': 'Rejected'}
+
+            descriptionCheck = descriptionChecker(carDesc)
+
+            return {'Id': ID, 'CheckedDescription': descriptionCheck,'RejectedImages': rejectedImages, 'adStatus': 'Approved'}
+        else:
+            return {"Response": "No images available for this car ad."}
+
+    except Exception as e:
+        print (f"Error inside the carAdMain Function {e}")
+    
+
+
+
+
---
+++ mongodb.py
@@ -0,0 +1,13 @@
+from pymongo import MongoClient
+
+connection_string = "mongodb+srv://aicarz:kxnJuY2Vc1UtHYVF@cluster0.uarux4m.mongodb.net/?retryWrites=true&w=majority"
+
+mongodbConn = MongoClient(connection_string)
+    
+carzdb = mongodbConn["aicarsdb"]
+carzcollection = carzdb["cars"]
+print("mongodb connected")
+
+
+# userdb = mongodbConn["aicarsdb"]
+# usercollection = userdb["users"]
