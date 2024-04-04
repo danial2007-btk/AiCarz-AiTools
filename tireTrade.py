54e35876c07cd9d63741807281663038022cdef2
hurairah4321 <hurairah.btkit@gmail.com>
2024-04-04 09:54:01 +0500

    change tireTrade

--- tireTrade.py
+++ tireTrade.py
@@ -2,24 +2,25 @@ import numpy as np
 from io import BytesIO
 from keras.utils import get_file
 from keras.models import load_model
-from keras.preprocessing import image
+from PIL import Image
 
-# Load your pre-trained model
-# model = load_model('tyre.h5')
 
 # URL of the Keras model in HDF5 format
-H5_url = "https://aitoolmodel.s3.eu-west-2.amazonaws.com/models/tyre.h5"
+H5_url = "https://aitoolmodel.s3.eu-west-2.amazonaws.com/models/tyre_checker.h5"
 
 # Load the Keras model
-model = load_model(get_file("tyre.h5", H5_url))
+model = load_model(get_file("tyre_checker.h5", H5_url), compile=False)
+
+# model = load_model("tyre_checker.h5", compile=False)
+
 
 def preprocess_image(contents):
     try:
-        # Convert the image file to a numpy array
-        img = image.load_img(BytesIO(contents), target_size=(224, 300))
-        img_array = image.img_to_array(img)
+        img = Image.open(BytesIO(contents)).convert('RGB')
+        img = img.resize((224, 224))
+        img_array = np.array(img)
         img_array = np.expand_dims(img_array, axis=0)
-        img_array /= 255.0
+        img_array = img_array.astype('float32') / 255.0
         return img_array
     except Exception as e:
         raise ValueError(str(e))
@@ -27,6 +28,12 @@ def preprocess_image(contents):
 def predict_image(img_array):
     # Make a prediction
     predictions = model.predict(img_array)
-    predicted_class = 1 if predictions[0] > 0.5 else 0
-    return "Good" if predicted_class == 1 else "Defected"
+    print(predictions)
+    predicted_class = np.argmax(predictions)
+    if predicted_class == 0:
+        return "Defected"
+    elif predicted_class == 1:
+        return "Good"
+    elif predicted_class == 2:
+        return "Not Tyre"
 
